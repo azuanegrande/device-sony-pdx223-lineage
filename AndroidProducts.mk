4ca17cfb6fe9dd40d5ec694f53f0bef33afcefdc
azuanegrande <43967574+azuanegrande@users.noreply.github.com>
2025-02-17 21:01:50 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/aosp_xqct54.mk \
-                     $(LOCAL_DIR)/lineage_pdx223.mk
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_pdx223.mk
 
 COMMON_LUNCH_CHOICES += \
     aosp_xqct54-eng \
